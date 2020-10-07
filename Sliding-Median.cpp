42faa9c9908c0da97443605326d03b0ed797be98
Hari <buzz.hari@gmail.com>
2020-10-07 20:25:19 +0530

    Partially Solved

---
+++ Sliding-Median.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <array>
+#include <climits>
+#include <iterator>
+
+
+// Defines for some data types. 
+#define li long int
+#define ll long long int
+#define ar array
+
+// Define for some utility function. 
+#define MAX(a,b) a > b ? a : b
+#define MIN(a,b) a < b ? a : b
+
+using namespace std;
+
+//int main() {
+    
+    //ll n, k;
+    //cin >> n >> k;
+    //cin >> n; 
+    //ll arr[n];
+    //for (int i = 0; i < n; ++i) {
+        //cin >> arr[i];
+    //}
+
+    //multiset<ll> nums;
+    //int j = 0;
+    //while(j < k)
+        //nums.insert(arr[j++]);
+
+    //for (int i = j; i < n; ++i) {
+        
+        //auto it = nums.begin();
+        //if (k%2)
+            //advance(it, k/2);
+        //else 
+            //advance(it, k/2-1);
+        //cout << *it << " ";
+        //nums.erase(nums.find(arr[i-k]));
+        //nums.insert(arr[i]);
+    //}
+    
+    //auto it = nums.begin();
+    //if (k%2)
+        //advance(it, k/2);
+    //else 
+        //advance(it, k/2-1);
+    //cout << *it << " ";
+    //return 0;
+//}
+
+int main() {
+    
+
+}
+

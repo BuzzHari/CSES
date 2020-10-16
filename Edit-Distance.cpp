19a83e693a9b2dc728773cfb6c90e9c42667843e
Hari <buzz.hari@gmail.com>
2020-10-16 21:10:11 +0530

    Solved

--- Edit-Distance.cpp
+++ Edit-Distance.cpp
@@ -29,20 +29,34 @@ int main() {
     cin >> x;
     cin >> y;
     
+    // Let dist[i][j] be the minimum number of moves to change x[:i] to y[:j].
+    // where, x[:i] means the first characters of x[:i].
+    
     dist[0][0] = 0;
     for (int i = 0; i <= x.size(); ++i) {
         for (int j = 0; j <= y.size(); ++j) {
             if (i || j)
                 dist[i][j] = INT_MAX;
             if (i)
+                // This is basically deleteing a character from x.
+                // but we still have to modify the remaining character.
+                // hence, 1+dist[i-1][j].
                 dist[i][j] = min(dist[i][j], dist[i-1][j]+1);
             if (j)
+                // This is basically inserting a character to end of x.
             dist[i][j] = min(dist[i][j], dist[i][j-1]+1);
-
+                
+                // This case is when we are transitioning from one character to another.
+                // The charcaters can be equal so it'll just by dist[i-1][j-1].
+                // if different add 1 as cost. 
+            
             if ((i && j) && x[i-1] == y[j-1])
                 dist[i][j] = min(dist[i][j], dist[i-1][j-1]);
             else if ((i&&j) && x[i-1] != y[j-1]) 
                 dist[i][j] = min(dist[i][j], dist[i-1][j-1]+1);
+            
+            // Those are the 4 cases we have, the min of all four will be the minimum distance
+            // required for d[i][j].
         }
     }
 

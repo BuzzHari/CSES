3509e5d661391f3a44d22f429b70284e476dcf73
Hari <buzz.hari@gmail.com>
2020-10-18 22:26:25 +0530

    In Progress

---
+++ Removal-Game.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <algorithm>
+#include <set>
+#include <map>
+#include <array>
+#include <climits>
+
+using namespace std;
+
+// Defines for some data types. 
+#define ll long long int
+#define uint unsigned int
+#define ar array
+
+// Macors for some utility function. 
+#define MAX(a,b) (a) > (b) ? (a) : (b)
+#define MIN(a,b) (a) < (b) ? (a) : (b)
+
+const uint MOD =  1000000000+7;
+
+
+int main() {
+    
+    int n; 
+    cin >> n;
+    
+    ll arr[n];
+
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    
+    return 0;
+}
+
+
